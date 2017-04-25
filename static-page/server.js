361d6d7e01e3d349a523c7f5081de1c52b6fe423
“gacardoso” <gcardosoti@gmail.com>
2017-04-24 21:32:21 -0300

    static-page

---
+++ static-page/server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Olá, mundo!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Olá, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Servidor rodando em: ${server.info.uri}`);
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
